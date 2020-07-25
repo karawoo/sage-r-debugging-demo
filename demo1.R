5b0f16de3dfb759d561e7754dd3eafdc7dbed6af
Kara Woo <kara.woo@sagebase.org>
2020-07-24 17:03:07 -0700

    Add browser() example inline

--- demo1.R
+++ demo1.R
@@ -16,12 +16,19 @@ f(2)
 
 # browser() ----
 
+h <- function(x) {
+  x <- x + 1
+  browser()
+  stop("An error occurred!", call. = FALSE)
+}
+
 f(1)
 
 # see also: rstudio's breakpoints, options(error = recover)
 
 # rlang tools ----
 
+source(here::here("functions.R")) # back to original (no browser())
 options(error = rlang::entrace)
 f(1)
 rlang::last_trace()
